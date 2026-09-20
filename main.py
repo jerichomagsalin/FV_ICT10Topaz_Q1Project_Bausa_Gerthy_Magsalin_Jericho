b60dca1a928c2a1718e059f987b1b5889d7950ab
jerichomagsalin <magsalin.jet@obmontessori.edu.ph>
2026-09-21 07:11:54 +0800

    sum

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 from pyscript import document, display
 
-def adding_numbers(e):
-    document.getElementById("output1").innerHTML = "" # clears previous output
-    num1 = float(document.getElementById('input1').value) # get 1st input
-    num2 = float(document.getElementById('input2').value) # get 2nd input
-    result = num1 + num2 # use operator to compute
-    display(result, target = "output1") #display output in div
+# Python is still deactivated and disconnected. Once the Main element and styling is finished, the python can happen:
 
-def create_order(e):
-    document.getElementById("output2").innerHTML = "" # clears previous output
-    prod1= document.getElementById("item1")
+
+
+# def adding_numbers(e):
+#   document.getElementById("output1").innerHTML = "" # clears previous output
+#   num1 = float(document.getElementById('input1').value) # get 1st input
+#   num2 = float(document.getElementById('input2').value) # get 2nd input
+#   result = num1 + num2 # use operator to compute
+#   display(result, target = "output1") #display output in div
+
+
+# def create_order(e):
+#   document.getElementById("output2").innerHTML = "" # clears previous output
+#   prod1 = document.getElementById("item1")
     # Calculate
-    subtotal = float(prod1.value) * prod1.checked
-    size = document.querySelector('input[name="size"]:checked')
-    size_price = float(size.value)
-    grandtotal = subtotal + size_price #add current and upsize price
-    display(subtotal, size_price, target="output2")
+#   subtotal = float(prod1.value) * prod1.checked
+#   size = document.querySelector('input[name="size"]:checked')
+#   size_price = float(size.value)
+#   grandtotal = subtotal + size_price #add current and upsize price
+#   display(subtotal, size_price, target="output2")
 
 def place_order(e):
     document.getElementById("output3").innerHTML = "" # clears previous output
     coffee = document.getElementById("coffee")
     coffee_price = float(coffee.value)
     display(coffee_price, target="output3")
+
+def initial_order (e):
+    document.getElementById("initialorder").innerHTML = ""
+    popsicle = float(document.getElementById('popsicle').value)
+    c_juice = float(document.getElementById('c_juice').value)
+    m_shake = float(document.getElementById('m_shake').value)
+    hc_sandwich = float(document.getElementById('hc_sandwich').value)
+    hotdog = float(document.getElementById('hotdog').value)
+
+    subtotal = popsicle * popsicle.checked + c_juice * c_juice.checked + m_shake * m_shake.checked + hc_sandwich * hc_sandwich.checked + hotdog * hotdog.checked
+
+    vat = subtotal * 0.12
+    
+    total = subtotal + vat
+
+    display(total, target="initialorder")
+    
+
+def create_sku(e):
+    document.getElementById("output1").innerHTML = ""
+
+
+    
