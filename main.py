f83bef38107a0b321874daade7864b41b054050b
jerichomagsalin <magsalin.jet@obmontessori.edu.ph>
2026-09-22 14:49:35 +0800

    ssum

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-from pyscript import document, display
+from pyscript import display
 
-# def adding_numbers(e):
-#   document.getElementById("output1").innerHTML = "" # clears previous output
-#   num1 = float(document.getElementById('input1').value) # get 1st input
-#   num2 = float(document.getElementById('input2').value) # get 2nd input
-#   result = num1 + num2 # use operator to compute
-#   display(result, target = "output1") #display output in div
-
-
-# def create_order(e):
-#   document.getElementById("output2").innerHTML = "" # clears previous output
-#   prod1 = document.getElementById("item1")
-    # Calculate
-#   subtotal = float(prod1.value) * prod1.checked
-#   size = document.querySelector('input[name="size"]:checked')
-#   size_price = float(size.value)
-#   grandtotal = subtotal + size_price #add current and upsize price
-#   display(subtotal, size_price, target="output2")
-
-# def place_order(e):
-#   document.getElementById("output3").innerHTML = "" # clears previous output
-#   coffee = document.getElementById("coffee")
-#   coffee_price = float(coffee.value)
-#   display(coffee_price, target="output3")
+# Python functions need to be used to generate the Receipt and to generate the SKU.
 
 def initial_order (e):
     document.getElementById("initialorder").innerHTML = ""
