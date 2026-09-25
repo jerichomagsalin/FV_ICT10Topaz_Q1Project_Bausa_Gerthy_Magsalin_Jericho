3be39329602213360314b999e69ef42c0eaea652
jerichomagsalin <magsalin.jet@obmontessori.edu.ph>
2026-09-25 12:01:44 +0800

    sum

--- main.py
+++ main.py
@@ -3,32 +3,40 @@ from pyscript import display, document
 
 # Python functions need to be used to generate the Receipt and to generate the SKU.
 
+popsicle = 50 
+c_juice = 100
+m_shake = 75
+hc_sandwich = 120
+hotdog = 100
+
 def initial_order (e):
-    document.getElementById("initialorder").innerHTML = ""
+    document.getElementById("subtotal").innerHTML = ""
+    document.getElementById("vat").innerHTML = ""
+    document.getElementById("total").innerHTML = ""
 
-    popsicle = float(document.getElementById('popsicle').value)
-    c_juice = float(document.getElementById('c_juice').value)
-    m_shake = float(document.getElementById('m_shake').value)
-    hc_sandwich = float(document.getElementById('hc_sandwich').value)
-    hotdog = float(document.getElementById('hotdog').value)
-
-    # affirming checkbox value (issue)
-    # problem, the background shifts when create order is pressed
-
-    # subtotal = (popsicle * popsicle.checked) + (c_juice * c_juice.checked) + (m_shake * m_shake.checked) +  (hc_sandwich * hc_sandwich.checked) + (hotdog * hotdog.checked)
-
-    subtotal = popsicle # for now
+    subtotal = float((popsicle * document.getElementById('popsicle').value)) + float((c_juice * document.getElementById('c_juice').value)) + float((m_shake * document.getElementById('m_shake').value)) + float((hc_sandwich * document.getElementById('hc_sandwich').value)) + float((hotdog * document.getElementById('hotdog').value))
 
     vat = subtotal * 0.12
-    
+        
     total = subtotal + vat
 
-    document.getElementById("initialorder").innerHTML = total
-    display(subtotal, target = "initialorder")
+    display(subtotal, target = "subtotal")
+    display(vat, target = "vat")
+    display(total, target = "total")
    
     
 def create_sku(e):
-    document.getElementById("create_sku").innerHTML = ""
+    document.getElementById("sku").innerHTML = ""
+    # document.getElementById("grouplabel").innerHTML = ""
+    
+    sample = document.getElementById('sample').value
+    productname = document.getElementById('productname').value
+    productnumber = document.getElementById('productnumber').value
 
+    sku_value = sample + "-" + productname[0:4].upper() + "-" + str(productnumber)
+    # groupvalue = "|" + document.getElementById('sample').id + ", " + sample + ", " + sample + "|"
+
+    # display(groupvalue, target = "grouplabel")
+    display(sku_value, target = "sku")
 
     
